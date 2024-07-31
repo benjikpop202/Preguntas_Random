38d07f356ea13b68c8782eeffb462952f87b861f
benjikpop202 <benjaminsobarzo2018@gmail.com>
2024-07-31 11:23:02 -0300

    actualizaciones

--- src/api/REST-API.js
+++ src/api/REST-API.js
@@ -1,5 +1,7 @@
-import express from 'express';
-import path from 'path';
+
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
 
 const app = express();
 const port = 3000;
@@ -10,6 +12,27 @@ app.set('views', path.join(__dirname, '../views'));
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../')));
+app.get('/', (req, res)=>{
+  try {
+    res.render('../views/index.ejs')
+  } catch (error) {
+    console.log(error);
+  }
+})
+app.get('/juego', (req, res)=>{
+  try {
+    res.render('../views/juego.ejs')
+  } catch (error) {
+    console.log(error);
+  }
+})
+app.get('/almacenar', (req, res)=>{
+  try {
+    res.render('../views/almacenar.ejs')
+  } catch (error) {
+    console.log(error);
+  }
+})
 
 app.listen(port, () => {
   console.log("Escuchando en el puerto:", port, "🚀");
